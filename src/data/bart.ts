37e2de1fac6e85055e458910e309bd9941f497fd
4rr0w <himanshu122122@gmail.com>
2021-07-26 20:27:33 +0530

    change bart data

--- src/data/bart.ts
+++ src/data/bart.ts
@@ -3,12 +3,11 @@ import getLifeArr from "./generateBartData";
 // eslint-disable-next-line import/prefer-default-export
 export const bartText = {
   money: "Rs",
-  collect: "Press to Collect Rs.",
-  pump: "Press this to pump up the balloon.",
+  collect: "Collect Rs.",
+  pump: "Pump Ballon",
   total: "Total Earned",
   last: "Last Balloon",
-  lastPage:
-    "Thank you for participating in the task. Please click here for the next task.",
+  lastPage: "Please click here for the next task.",
   startingAmount: 0,
   perPushAmount: 10,
 };
