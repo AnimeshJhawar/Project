72b127736c80e8a10f4b69461bd8388f28498383
4rr0w <4rr0w.v@gmail.com>
2021-08-15 21:34:22 +0530

    add dummysurveys

--- src/data/survey.data.ts
+++ src/data/survey.data.ts
@@ -6,9 +6,9 @@ export const iframeScreen = {
 
 export const surveys = [
   "https://formfacade.com/include/118024647094610445616/form/1FAIpQLSfLPfHYKBTETf-q-1nl5-clNCFYESPp5ibXWUbPAJEH5oOcqQ/classic.js?div=ff-compose",
-  "https://docs.google.com/forms/d/e/1FAIpQLSfqmrfBjcevRJ7AE1WVN8klgFWRxx1NPcMASPYjF_PzY04tsA/viewform",
-  "https://docs.google.com/forms/d/e/1FAIpQLSdpDZbrorxl8OidMV62esWnQfoXlhpdcuPrkzEuHUk1mJm71Q/viewform",
-  "https://docs.google.com/forms/d/e/1FAIpQLScjECktriBKB6QKCXELlT_cFP2dEbqRt_nWM9DfJktOtnsmTg/viewform",
-  "https://docs.google.com/forms/d/e/1FAIpQLScNEyg9KzHIaZfflloV1rezQY8yVN-jR3i1nM8ZS6bZ0qN0zg/viewform",
-  "https://docs.google.com/forms/d/e/1FAIpQLSeCO9mOZVJIvJxFdc7xKqT2NOHQc2TmW1vGQj7RhwOXd3aGZw/viewform",
+  "https://formfacade.com/include/118024647094610445616/form/1FAIpQLSfLPfHYKBTETf-q-1nl5-clNCFYESPp5ibXWUbPAJEH5oOcqQ/classic.js?div=ff-compose",
+  "https://formfacade.com/include/118024647094610445616/form/1FAIpQLSfLPfHYKBTETf-q-1nl5-clNCFYESPp5ibXWUbPAJEH5oOcqQ/classic.js?div=ff-compose",
+  "https://formfacade.com/include/118024647094610445616/form/1FAIpQLSfLPfHYKBTETf-q-1nl5-clNCFYESPp5ibXWUbPAJEH5oOcqQ/classic.js?div=ff-compose",
+  "https://formfacade.com/include/118024647094610445616/form/1FAIpQLSfLPfHYKBTETf-q-1nl5-clNCFYESPp5ibXWUbPAJEH5oOcqQ/classic.js?div=ff-compose",
+  "https://formfacade.com/include/118024647094610445616/form/1FAIpQLSfLPfHYKBTETf-q-1nl5-clNCFYESPp5ibXWUbPAJEH5oOcqQ/classic.js?div=ff-compose",
 ];
