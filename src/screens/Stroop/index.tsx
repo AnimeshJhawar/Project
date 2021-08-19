d2ece7bf501abe85e02f8c4834a68bf4c8b725fd
4rr0w <4rr0w.v@gmail.com>
2021-08-19 17:59:56 +0530

    change stroop bg to black

--- src/screens/Stroop/index.tsx
+++ src/screens/Stroop/index.tsx
@@ -47,7 +47,6 @@ export const Stroop: React.FC<StroopProps> = () => {
             <p>
               <b>{stroopInstructions.remember}</b>
             </p>
-            {/* t{" "} */}
             <ul>
               {stroopInstructions.points.map((point) => (
                 <li>{point}</li>
