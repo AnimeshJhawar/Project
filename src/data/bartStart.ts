75d49de90968fe81bc0c1ad7cd9f7516d35b8e7a
4rr0w <4rr0w.v@gmail.com>
2021-10-18 03:53:30 +0530

    add hindi text

--- src/data/bartStart.ts
+++ src/data/bartStart.ts
@@ -15,15 +15,15 @@ export const bartStartData = {
 
 /* eslint-disable import/prefer-default-export */
 export const bartStartDataHindi = {
-  heading: "Start the Task",
-  rem: "Remember these rules:",
+  heading: "खेल शुरू करे ",
+  rem: "इन नियमों को याद रखें:",
   points: [
-    "Click the button “Pump it” to fill air/pump the balloon.",
-    "Balloon can burst any time, so play strategically.",
-    "Different balloon have different bursting point.",
-    "Don’t forget to collect money by clicking “Collect Rs” before the balloon burst.",
-    "Try to collect as much money as possible.",
+    "गुब्बारे में  हवा भरने के लिए 'पंप करे' बटन पर क्लिक करें।",
+    "गुब्बारा कभी भी फट सकता है, इसलिए रणनीतिक रूप से खेलें। ",
+    "हर गुब्बारे का फूटने  का स्तर (बिंदु) अलग हैं।",
+    "गुब्बारा फूटने से पहले 'रुपये इकट्ठा करे' वाले बटन पर क्लिक करके पैसे  इकट्ठा करना न भूलें।",
+    "ज्यादा से ज्यादा पैसा इकट्ठा करने की कोशिश करें।",
   ],
-  startText: "Start when you are ready",
-  next: " Play the Game",
+  startText: "जब आप तैयार हों तब शुरू करें ।",
+  next: "खेल खेलने के लिए",
 };
